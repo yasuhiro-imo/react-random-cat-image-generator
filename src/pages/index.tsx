20b8a4195b8664a0c4788c619343dced807607bc
yasuhiro-imo <yasuhiro.imou@gmail.com>
2022-12-15 19:42:09 +0900

    make variable-types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,37 @@
 import { useState } from "react"
 
-const fetchCatImage = async () => {
-    const res = await fetch("https://api.thecatapi.com/v1/images/search")
-    const result = (await res.json())
+interface CatCategory {
+    id: number
+    name: string
+}
+
+interface SearchCatImage {
+    breeds: string[]
+    catCategories: CatCategory[]
+    id: string
+    url: string
+    width: number
+    height: number
+}
+
+type SearchCatImageResponse = SearchCatImage[]
+
+const fetchCatImage = async (): Promise<SearchCatImage> => {
+    const response = await fetch("https://api.thecatapi.com/v1/images/search")
+    const result = (await response.json()) as SearchCatImageResponse
     return result[0]
 }
 
-const IndexPage = () => {
+const IndexPage = (): JSX.Element => {
     const [catImageUrl, setCatImageUrl] = useState("https://cdn2.thecatapi.com/images/bpc.jpg")
-    const handleClick = async () => {
+    const handleClick = async (): Promise<void> => {
         const image = await fetchCatImage()
         setCatImageUrl(image.url)
     }
 
     return (
         <div>
-            <button onClick={handleClick}>🐱 Generate New Cat Image 🐱</button>
+            <button onClick={handleClick}>🐱 Fetch New Cat Image 🐱</button>
             <div style={{ marginTop : 8 }}>
                 <img src={catImageUrl} />
             </div>
