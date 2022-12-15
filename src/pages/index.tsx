c39e5f2f92133c57de185ed994fe1c3952102b58
yasuhiro-imo <yasuhiro.imou@gmail.com>
2022-12-15 16:25:16 +0900

    add random cat image generator

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+const catImages: string[] = [
+    "https://cdn2.thecatapi.com/images/bpc.jpg",
+    "https://cdn2.thecatapi.com/images/eac.jpg",
+    "https://cdn2.thecatapi.com/images/6qi.jpg",
+]
+
+const randomCatImage = (): string => {
+    const index = Math.floor(Math.random() * catImages.length)
+    return catImages[index]
+}
+
+const IndexPage = () => {
+    const [catImageUrl, setCatImageUrl] = useState("https://cdn2.thecatapi.com/images/bpc.jpg")
+    const handleClick = () => setCatImageUrl(randomCatImage())
+
+    return (
+        <div>
+            <button onClick={handleClick}>🐱Random Cat Image🐱</button>
+            <div style={{ marginTop: 8 }}>
+                <img src={catImageUrl} />
+            </div>
+        </div>
+    );
+};
+
+export default IndexPage;
