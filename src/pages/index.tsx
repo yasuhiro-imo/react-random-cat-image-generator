0c4727743a700084b330d8e422dc2f8a4a5aa5d3
yasuhiro-imo <yasuhiro.imou@gmail.com>
2022-12-15 16:27:29 +0900

    use api.thecatapi.com

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,26 @@
 import { useState } from "react"
 
-const catImages: string[] = [
-    "https://cdn2.thecatapi.com/images/bpc.jpg",
-    "https://cdn2.thecatapi.com/images/eac.jpg",
-    "https://cdn2.thecatapi.com/images/6qi.jpg",
-]
-
-const randomCatImage = (): string => {
-    const index = Math.floor(Math.random() * catImages.length)
-    return catImages[index]
+const fetchCatImage = async () => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search")
+    const result = (await res.json())
+    return result[0]
 }
 
 const IndexPage = () => {
     const [catImageUrl, setCatImageUrl] = useState("https://cdn2.thecatapi.com/images/bpc.jpg")
-    const handleClick = () => setCatImageUrl(randomCatImage())
+    const handleClick = async () => {
+        const image = await fetchCatImage()
+        setCatImageUrl(image.url)
+    }
 
     return (
         <div>
-            <button onClick={handleClick}>🐱Random Cat Image🐱</button>
-            <div style={{ marginTop: 8 }}>
+            <button onClick={handleClick}>🐱 Generate New Cat Image 🐱</button>
+            <div style={{ marginTop : 8 }}>
                 <img src={catImageUrl} />
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default IndexPage;
+export default IndexPage
